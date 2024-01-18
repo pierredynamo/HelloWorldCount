bf5c8e99fbe3544cb627f28c2e3d287e0bd6abd3
Pierre Fotsing <pierredynamo@yahoo.fr>
2024-01-18 00:05:58 -0700

    modif in main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from module import Visitors
 
